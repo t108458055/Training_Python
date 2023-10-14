5a086321a0887bc4e631b926da16b4234e4d3d82
t108458055 <t108458055@gmail.com>
2023-10-15 00:47:01 +0800

    Books first commit

---
+++ BookFlagPy/Ch1_helloPy.py
@@ -0,0 +1,22 @@
+# introduction
+print("Hello World!")
+print("Happy BirthDay")
+print(5+2)
+# 圓柱體積公式 V= pi*r^2 *h
+print("圓柱體積為：",3.14*10*10*5)
+print("圓柱表面積為：",3.14*10*10*2+2*3.14*10*5)
+# 借100萬 年利率單利6%,借款期限3年
+# 單利本利和公式 :本金*(1+年利率*年)
+print("單利本利和為",10000000*(1+0.06*3))
+# 複利本利和公式 :本金*(1+年利率)^年
+print("複利本利和為",10000000*(1+0.06)*(1+0.06)*(1+0.06))
+
+# 安裝套件
+# conda install 套件名稱 conda install NumPy
+# 更新套件
+# conda update 套件名稱 conda update NumPy
+# 移除套件
+# conda uninstall 套件名稱 conda uninstall NumPy
+# 建議寫法
+i = 100
+print(i)
---
+++ BookFlagPy/Ch1_practice.py
@@ -0,0 +1,8 @@
+# 印出 1 加 10的結果
+def SumTotal():
+  j = 0
+  for i in range(1,11):
+    j += i
+  return j
+
+print(SumTotal())
