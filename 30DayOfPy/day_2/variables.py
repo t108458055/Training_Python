e4c3cff345f95fb6be310a71345bc215420ab96a
AndyHsiao <t108458055@gmail.com>
2024-03-02 23:30:46 +0800

    Day2 variables

---
+++ 30DayOfPy/day_2/variables.py
@@ -0,0 +1,68 @@
+#　2024-03-02  practice 
+
+#
+
+# Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
+# Write a python comment saying 'Day 2: 30 Days of python programming'
+"""_summary_
+Day 2 : 30 Days of python programming
+    pass
+"""
+# Declare a first name variable and assign a value to it
+first_name = 'Andy'
+# Declare a last name variable and assign a value to it
+last_name = 'Xaio'
+# Declare a full name variable and assign a value to it
+full_name = first_name + last_name
+# Declare a country variable and assign a value to it
+country = 'Taiwan'
+# Declare a city variable and assign a value to it
+city = 'Taipei'
+# Declare an age variable and assign a value to it
+age = 31
+# Declare a year variable and assign a value to it
+year = 2024
+# Declare a variable is_married and assign a value to it
+is_married = False
+# Declare a variable is_true and assign a value to it
+is_true = False
+# Declare a variable is_light_on and assign a value to it
+is_light_on = True
+# Declare multiple variable on one line
+first_name, last_name, country, age, is_married = 'Andy', 'Xaio', 'Taiwan', 31, False
+
+# Check the data type of all your variables using type() built-in function
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name, 'Type:' ,type(first_name))
+print('Last name: ', last_name, 'Type:' ,type(last_name))
+print('Country: ', country, 'Type:' ,type(country))
+print('Age: ', age, 'Type:' ,type(age))
+print('Married: ', is_married, 'Type:' ,type(is_married))
+# Using the len() built-in function, find the length of your first name
+print('First name length:', len(first_name))
+# Compare the length of your first name and your last name
+# Declare 5 as num_one and 4 as num_two
+num_one, num_two = 5, 4
+# Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+# Subtract num_two from num_one and assign the value to a variable diff
+diff = num_two - num_one
+# Multiply num_two and num_one and assign the value to a variable product
+product = num_two * num_one
+# Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two // num_one
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+
+# The radius of a circle is 30 meters.
+circle = 30
+# Calculate the area of a circle and assign the value to a variable name of area_of_circle
+area_of_circle = pi * circle^2
+# Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = pi * 2 * circle
+# Take radius as user input and calculate the area.
+# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+# Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
